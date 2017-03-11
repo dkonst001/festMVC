95ee42e47fd4adcc48d54c00bcd04b46f32420ff
danny <danny.konstadt@gmail.com>
2017-03-11 21:46:49 +0200

    Add Start and End Time to EventViewModel

---
+++ FestMVC/Migrations/201703111437026_Delete Start Time and End Time from Events.cs
@@ -0,0 +1,16 @@
+namespace FestMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteStartTimeandEndTimefromEvents : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
